eb121a93b0726304a249c89637297488ba22364a
umeshkanoja <umeshkanoja26@gmail.com>
2024-07-30 19:51:50 -0700

    removing unused imports

--- src/main/java/com/exercise/ledger/repository/account/AccountRepoAccessor.java
+++ src/main/java/com/exercise/ledger/repository/account/AccountRepoAccessor.java
@@ -1,7 +1,6 @@
 package com.exercise.ledger.repository.account;
 
 import java.util.List;
-import java.util.UUID;
 
 import com.exercise.ledger.core.account.Account;
 import com.exercise.ledger.core.customer.Customer;
--- src/main/java/com/exercise/ledger/repository/account/AccountRepoAccessorImpl.java
+++ src/main/java/com/exercise/ledger/repository/account/AccountRepoAccessorImpl.java
@@ -1,7 +1,6 @@
 package com.exercise.ledger.repository.account;
 
 import java.util.List;
-import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
